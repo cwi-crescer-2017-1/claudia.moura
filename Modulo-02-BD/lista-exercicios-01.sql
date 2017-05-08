94aac478a0b2efca16f869803f80663d565888af
ClaudiaStrm <s_moura@hotmail.com>
2017-05-08 01:09:55 -0300

    refactor(exercicios): lista 01 feita no SQL Server

--- Modulo-02-BD/lista-exercicios-01.sql
+++ Modulo-02-BD/lista-exercicios-01.sql
@@ -1,17 +1,11 @@
-/* Lista de exercícios 1*/
-
-/* Exercício 01*/
+﻿/* --Lista de exercícios 1
+--Exercício 01
 CREATE TABLE CidadeAux SELECT * FROM Cidade;
-
-
-/* Exercício 02*/
+--Exercício 02
 truncate TABLE CidadeAux;
-
 insert into CidadeAux select * FROM Cidade
 WHERE UF = 'RS';
-
-
-/* Exercício 03*/
+--Exercício 03
 Create table Produto 
 (
   IDProduto  		int         not null,
@@ -23,15 +17,69 @@ Create table Produto
   Preco				double,  
     constraint PK_IDProduto primary key (IDProduto)
 );	
-
 alter table Produto
 modify DescricaoProduto varchar(200);
-
 alter table Produto
 modify LocalEstoque varchar(45) null,
 modify Quantidade int not null;
+--Exercício 04
+insert into Produto 
+      (IDProduto, 
+  NomeProduto, 
+  DescricaoProduto,
+  DataCriacao,
+  LocalEstoque,
+  Quantidade,
+  Preco)
+  values 
+      (1, 
+       'Arroz', 
+       'Saco de Arroz de 1 kg do tipo parbolizado',
+       ('2017/04/28'), 
+       'Alimentícios',
+       99,
+       4.99);
+insert into Produto 
+(
+  IDProduto, 
+  NomeProduto, 
+  DescricaoProduto,
+  Quantidade,
+  Preco)
+  values 
+  (2,
+  'Feijao',
+  'Saco de feijão preto de 0,5kg',
+  22,
+  6.99);*/
 
-/* Exercício 04*/
+
+
+-- Exercícios SQL Server
+
+--Crie a tabela CidadeAux a partir de uma cópia da tabela Cidade. Considere todos os registros existentes.
+Select * into CidadeAux from Cidade;
+
+--Limpe a tabela CidadeAux (Truncate table CidadeAux). E em seguida insira todas os registros da tabela Cidade onde o estado seja RS.
+Truncate table CidadeAux;
+Insert into CidadeAux select * from Cidade
+Where UF = 'RS';
+
+--Crie uma tabela para armazenar informações de produtos. Devem ter os seguintes atributos:
+Create table Produto 
+(
+  IDProduto  		int         not null,
+  NomeProduto		varchar(30) not null,
+  DescricaoProduto  varchar(200)not null,
+  DataCriacao		date,
+  LocalEstoque		varchar(45),
+  Quantidade		int			not null,
+  Preco				float,
+  );
+
+ALTER TABLE Produto add constraint PK_IDProduto primary key (IDProduto)
+
+--Crie dois registros nessa tabela nova (produto).
 
 insert into Produto 
       (IDProduto, 
@@ -49,8 +97,6 @@ insert into Produto
        'Alimentícios',
        99,
        4.99);
---
-
 insert into Produto 
 (
   IDProduto, 
@@ -63,4 +109,4 @@ insert into Produto
   'Feijao',
   'Saco de feijão preto de 0,5kg',
   22,
-  6.99);
+6.99);
