25d8fccb44f4b7c3d65a827aec81eeda885d9160
ClaudiaStrm <s_moura@hotmail.com>
2017-05-21 05:01:27 -0300

    feat(aula03): finalizar exercicios de aula

--- Modulo-05-Angular/Aula03/form.js
+++ Modulo-05-Angular/Aula03/form.js
@@ -1,33 +1,40 @@
 let app = angular.module('angularjs', []);
 app.controller('MainController', function($scope){
 
-  $scope.nomes = ['Bernardo', 'Nunes'];
-  $scope.incluirNome = function(novoNome){
-    if($scope.form.$valid){
-       $scope.nomes.push(novoNome);
+  let instrutores = [
+    {
+      nome: 'Bernardo',
+      sobrenome: 'Rezende',
+      idade: 30,
+      email: 'bernardo@cwi.com.br',
+      jaDeuAula: true,
+      aula: 'OO'
     }
-}
+  ];
 
-let instrutores = [
-  {
-    nome: 'Bernardo',
-    sobrenome: 'Rezende',
-    idade: 30,
-    email: 'bernardo@cwi.com.br',
-    jaDeuAula: true,
-    aula: 'OO'
-  }
-];
-
-let aulas = [
-  'OO',
-  'HTML e CSS',
-  'Javascript',
-  'AngularJS',
-  'Banco de Dados I'
-];
+  let aulas = [
+    'OO',
+    'HTML e CSS',
+    'Javascript',
+    'AngularJS',
+    'Banco de Dados I'
+  ];
 
  $scope.aulas = aulas;
  $scope.instrutores = instrutores;
+
+
+  $scope.adicionarInstrutor = function(){
+    novoInstrutor = {
+      nome:$scope.novoInstrutor.nome,
+      sobrenome:$scope.novoInstrutor.sobrenome,
+      idade:$scope.novoInstrutor.idade,
+      email:$scope.novoInstrutor.email,
+      jaDeuAula:$scope.novoInstrutor.jaDeuAula || false,
+      aula:$scope.novoInstrutor.aula
+    };
+    $scope.instrutores.push(novoInstrutor);
+    alert('Cadastro efetivado com sucesso!')
+  };
 
 })
