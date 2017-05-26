c06c840b8fe57046f300db532860ffee68069606
Claudia SM <claudia.moura@cwi.com.br>
2017-05-26 17:26:43 -0300

    feat(Aula03): enviar exercícios feitos em aula

---
+++ Modulo-06-.NET/Aula03/ExemploWebApi/ExemploWebApi/Controllers/ExemploController.cs
@@ -0,0 +1,44 @@
+﻿using ExemploWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    public class HeroisController : ApiController
+    {
+        public static List<Heroi> Herois { get; set; } = new List<Heroi>();
+
+
+        public int contador = 1;
+        public IEnumerable<Heroi> Get(string nome = null, int? id = null)
+
+        {
+            //var herois = new List<Heroi>()
+            /*{
+                new Heroi() { Id = 1, Nome = "Goku", Poder = new Poder() { Nome = "Genki Dama", Dano = 8001 } },
+                new Heroi() { Id = 2, Nome = "Kenshin", Poder = new Poder() { Nome = "Hiten Mitsurugi Ryu", Dano = 489 } },
+                new Heroi() { Id = 3, Nome = "Madoka", Poder = new Poder() { Nome = "Deusa", Dano = 1000000 } },
+            };*/
+            
+            return Herois.Where(heroi =>
+            (heroi.Id == id || id == null) &&
+            (nome == null || heroi.Nome == nome));
+
+            /* if (id == null){ return herois;} return herois.Where(x => x.Id == id); //&& x.Nome == nome );*/
+        }
+
+        public IHttpActionResult Post(Heroi heroi)
+        {
+            if (heroi.Id == 0)
+                return BadRequest();
+            else
+                Herois.Add(heroi);
+                heroi.Id = contador++;
+                return Ok(heroi);
+        }
+    }
+}
